b882c40fca3ee4d91c1e1de044f2643cbfb65818
Chakradhar Reddy <chakrireddy170@gmail.com>
2025-11-06 12:12:21 +0530

    GITX commit

--- src/test/java/Tests/GitCheckUp.java
+++ src/test/java/Tests/GitCheckUp.java
@@ -5,6 +5,7 @@ public class GitCheckUp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GitCheckUpOne");
+		System.out.println("GitCheckUPTwo");
 
 	}
 
